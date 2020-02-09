34cee368057bf2c9d52ee0c6e290e4e0f9b6b030
Chad Miller <chadtmiller15@gmail.com>
2020-02-09 11:03:44 -0800

    Add countdown clock

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-import { Box, Heading } from '@chakra-ui/core';
+import { Box, Heading, Flex } from '@chakra-ui/core';
 
 import { get } from 'lodash/object';
 
@@ -9,7 +9,8 @@ import withAuthUserInfo from '../utils/pageWrappers/withAuthUserInfo';
 
 import Layout from '../components/layout';
 
-import { FaRegMap } from 'react-icons/fa';
+import { FaRegMap, FaRegClock } from 'react-icons/fa';
+import Countdown from 'react-countdown';
 
 const MapboxNoSSR = dynamic(
   () => import('../components/mapbox').then(mod => mod.Mapbox),
@@ -18,6 +19,15 @@ const MapboxNoSSR = dynamic(
   }
 );
 
+const countdownRenderer = ({ days, hours, minutes, seconds }) => {
+  return (
+    <Flex alignItems="center">
+      <Box as={FaRegClock} mr={3} /> {days} days {hours} hrs {minutes} min{' '}
+      {seconds} sec
+    </Flex>
+  );
+};
+
 const HomePage = props => {
   const { AuthUserInfo } = props;
   const AuthUser = get(AuthUserInfo, 'AuthUser', null);
@@ -25,6 +35,7 @@ const HomePage = props => {
     <Layout AuthUser={AuthUser}>
       <Box>
         <Box
+          position="relative"
           px={10}
           py={250}
           backgroundImage="url(/noluck-1.jpg)"
@@ -32,7 +43,22 @@ const HomePage = props => {
           backgroundSize="cover"
           color="white"
           bg="gray"
-        />
+        >
+          <Box
+            fontWeight="bold"
+            position="absolute"
+            right={10}
+            bottom={10}
+            background="black"
+            transform="skew(-5deg)"
+            p={3}
+          >
+            <Countdown
+              date={new Date('03/17/2020')}
+              renderer={countdownRenderer}
+            />
+          </Box>
+        </Box>
         <Box width="100%" maxWidth={960} mx="auto" px={10} paddingTop={3}>
           <Heading size="xl" display="flex" alignItems="center">
             Routes <Box ml={3} as={FaRegMap} />
