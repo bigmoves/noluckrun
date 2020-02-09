5a1d92f1655e51da87f47fdf4c87e584f9c31602
Chad Miller <chadtmiller15@gmail.com>
2020-02-09 13:04:45 -0800

    Add form validation

--- components/registration-form.js
+++ components/registration-form.js
@@ -8,42 +8,66 @@ import {
   Button,
   Select,
   Box,
-  RadioGroup,
-  Radio
+  Stat,
+  StatLabel,
+  StatNumber
 } from '@chakra-ui/core';
+
+import { keys } from 'lodash/object';
 
 import { checkout } from '../utils/stripe/checkout';
 
 export default function RegistrationForm() {
   const { handleSubmit, errors, register } = useForm();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [checkoutApiError, setCheckoutApiError] = useState(false);
 
+  console.log({ errors });
   function onSubmit(values) {
     console.log(values);
     setIsSubmitting(true);
 
-    checkout(values).then(() => {
-      setIsSubmitting(false);
-    });
+    // checkout(values)
+    //   .then(() => {
+    //     setIsSubmitting(false);
+    //   })
+    //   .catch(() => {
+    //     setIsSubmitting(false);
+    //     setCheckoutApiError(true);
+    //   });
   }
 
   return (
     <Box as="form" onSubmit={handleSubmit(onSubmit)} py={3} px={2}>
-      <FormControl mb={4} isRequired>
+      <FormControl mb={4} isInvalid={errors.firstName}>
         <FormLabel htmlFor="firstName">First Name</FormLabel>
-        <Input id="firstName" name="firstName" ref={register} />
+        <Input
+          id="firstName"
+          name="firstName"
+          ref={register({ required: true })}
+        />
+        <FormErrorMessage>Enter a first name</FormErrorMessage>
       </FormControl>
-      <FormControl mb={4} isRequired>
+      <FormControl mb={4} isInvalid={errors.lastName}>
         <FormLabel htmlFor="lastName">Last Name</FormLabel>
-        <Input id="lastName" name="lastName" ref={register} />
+        <Input
+          id="lastName"
+          name="lastName"
+          ref={register({ required: true })}
+        />
+        <FormErrorMessage>Enter a last name</FormErrorMessage>
       </FormControl>
-
-      <FormControl mb={4} isRequired>
+      <FormControl mb={4} isInvalid={errors.email}>
         <FormLabel htmlFor="email">Email</FormLabel>
-        <Input id="email" name="email" type="email" ref={register} />
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          ref={register({ required: true })}
+        />
+        <FormErrorMessage>Enter an email address</FormErrorMessage>
       </FormControl>
-
-      <FormControl mb={4} isRequired>
+      <FormControl mb={4} isInvalid={errors.shirtSize}>
         <FormLabel htmlFor="shirtSize" as="legend">
           T-Shirt Size
         </FormLabel>
@@ -51,16 +75,16 @@ export default function RegistrationForm() {
           id="shirtSize"
           name="shirtSize"
           placeholder="Select Size"
-          ref={register}
+          ref={register({ required: true })}
         >
           <option value="small">Small</option>
           <option value="medium">Medium</option>
           <option value="large">Large</option>
           <option value="x-large">X Large</option>
         </Select>
+        <FormErrorMessage>Select a size</FormErrorMessage>
       </FormControl>
-
-      <FormControl mb={4} isRequired>
+      <FormControl mb={4} isInvalid={errors.routeName}>
         <FormLabel htmlFor="routeName" as="legend">
           Route
         </FormLabel>
@@ -68,13 +92,19 @@ export default function RegistrationForm() {
           id="routeName"
           name="routeName"
           placeholder="Select Route"
-          ref={register}
+          ref={register({ required: true })}
         >
           <option value="5k">5k</option>
           <option value="10k">10k</option>
           <option value="15k">15k</option>
         </Select>
+        <FormErrorMessage>Select a route</FormErrorMessage>
       </FormControl>
+
+      <Stat mb={4}>
+        <StatLabel>Total:</StatLabel>
+        <StatNumber>$25.00</StatNumber>
+      </Stat>
 
       <Button type="submit" variantColor="purple" isLoading={isSubmitting}>
         Checkout
