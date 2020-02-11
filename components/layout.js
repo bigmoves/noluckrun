79fac0ee7814cc8ed9687d279cb487a3024e9fe1
Chad Miller <chadtmiller15@gmail.com>
2020-02-10 23:06:06 -0800

    Use next links

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import { Box, Flex, Button, Link } from '@chakra-ui/core';
+import { Box, Flex, Button, Link as ChakraLink } from '@chakra-ui/core';
 
 import logout from '../utils/auth/logout';
 import firebase from 'firebase/app';
 import Router from 'next/router';
+import Link from 'next/link';
 
 const renderAuthButtons = AuthUser => {
   return AuthUser ? (
@@ -40,28 +41,39 @@ const renderAuthButtons = AuthUser => {
 const Layout = ({ AuthUser, adminPage, children }) => (
   <Box>
     <Flex height={50} alignItems="center" justifyContent="space-between" px={2}>
-      <Link href="/" fontWeight="bold" fontSize={['sm', 'md', '3xl']}>
-        2020 No Luck Run
+      <Link href="/">
+        <ChakraLink href="/" fontWeight="bold" fontSize={['sm', 'md', '3xl']}>
+          2020 No Luck Run
+        </ChakraLink>
       </Link>
       {adminPage ? (
         renderAuthButtons(AuthUser)
       ) : (
-        <Button variantColor="purple" as="a" href="/register" fontWeight="bold">
-          Register
-        </Button>
+        <Link href="/register">
+          <Button
+            variantColor="purple"
+            as="a"
+            href="/register"
+            fontWeight="bold"
+          >
+            Register
+          </Button>
+        </Link>
       )}
     </Flex>
     {children}
-    <Flex
-      as="footer"
-      mt={8}
-      py={4}
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="purple.200"
-    >
-      Made with 💖 by Chad Miller
-    </Flex>
+    <Link href="/admin">
+      <Flex
+        as="footer"
+        mt={8}
+        py={4}
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="purple.200"
+      >
+        Made with 💖 by Chad Miller
+      </Flex>
+    </Link>
   </Box>
 );
 
